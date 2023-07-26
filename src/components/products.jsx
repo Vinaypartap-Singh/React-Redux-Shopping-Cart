25fc1185ab2e7d8bd347ed36b0c51ccb58b4b27c
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-07-26 13:25:23 +0530

    Added Price Filter

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,3 +1,109 @@
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchProducts } from "../store/productSlice";
+// import Card from "./Card";
+// import { categoryProduct } from "../store/categorySlice";
+
+// export default function Products() {
+//   const { data: allProducts, status: allProductStatus } = useSelector(
+//     (state) => state.product
+//   );
+
+//   const [categoryBtn, setCategoryBtn] = useState("smartphones");
+//   const [categories, setCategories] = useState([]);
+//   const [filteredItems, setFIlteredItems] = useState([]);
+
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(fetchProducts());
+//     dispatch(categoryProduct(categoryBtn));
+//   }, [dispatch]);
+
+//   useEffect(() => {
+//     fetch("https://dummyjson.com/products/categories")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setCategories(data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, []);
+
+//   useEffect(() => {
+//     fetch("https://dummyjson.com/products?limit=100")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setFIlteredItems(data.products);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, []);
+
+//   const updateCategory = (category) => {
+//     const fetProducts = fetch(
+//       `https://dummyjson.com/products/category/${category}`
+//     )
+//       .then((res) => res.json())
+//       .then((res) => setFIlteredItems(res.products));
+//   };
+
+//   if (categories.length === 0) {
+//     return <div>Loading categories...</div>;
+//   }
+
+//   if (allProductStatus === "loading") {
+//     return <h1>Loading Products</h1>;
+//   }
+
+//   return (
+//     <>
+//       <h1>{categoryBtn === "all" ? "All Products" : "Products"}</h1>
+//       <div
+//         style={{
+//           display: "flex",
+//           gap: "30px",
+//           flexWrap: "wrap",
+//         }}
+//       >
+//         <button
+//           style={{
+//             color: "white",
+//             textDecoration: "underline",
+//             textTransform: "capitalize",
+//           }}
+//           onClick={() => setFIlteredItems(allProducts.products)}
+//         >
+//           All
+//         </button>
+//         {categories.map((category, index) => (
+//           <button
+//             onClick={() => updateCategory(category)}
+//             key={index}
+//             style={{
+//               color: "white",
+//               textDecoration: "underline",
+//               textTransform: "capitalize",
+//             }}
+//           >
+//             {category}
+//           </button>
+//         ))}
+//       </div>
+//       <div className="filter">
+//         {/* Check if filteredProducts is not empty */}
+//         {filteredItems.length > 0 ? (
+//           filteredItems.map((data) => <Card data={data} key={data.id} />)
+//         ) : (
+//           <p>No products found for the selected category.</p>
+//         )}
+//       </div>
+//     </>
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../store/productSlice";
@@ -11,14 +117,16 @@ export default function Products() {
 
   const [categoryBtn, setCategoryBtn] = useState("smartphones");
   const [categories, setCategories] = useState([]);
-  const [filteredItems, setFIlteredItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchProducts());
     dispatch(categoryProduct(categoryBtn));
-  }, [dispatch]);
+  }, [dispatch, categoryBtn]);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products/categories")
@@ -35,7 +143,7 @@ export default function Products() {
     fetch("https://dummyjson.com/products?limit=100")
       .then((res) => res.json())
       .then((data) => {
-        setFIlteredItems(data.products);
+        setFilteredItems(data.products);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -47,7 +155,22 @@ export default function Products() {
       `https://dummyjson.com/products/category/${category}`
     )
       .then((res) => res.json())
-      .then((res) => setFIlteredItems(res.products));
+      .then((res) => setFilteredItems(res.products));
+  };
+
+  const handleFilter = () => {
+    let min = parseFloat(minPrice);
+    let max = parseFloat(maxPrice);
+
+    if (isNaN(min)) min = 0;
+    if (isNaN(max)) max = Infinity;
+
+    const filteredProducts = allProducts.products.filter(
+      (product) =>
+        parseFloat(product.price) >= min && parseFloat(product.price) <= max
+    );
+
+    setFilteredItems(filteredProducts);
   };
 
   if (categories.length === 0) {
@@ -74,13 +197,19 @@ export default function Products() {
             textDecoration: "underline",
             textTransform: "capitalize",
           }}
-          onClick={() => setFIlteredItems(allProducts.products)}
+          onClick={() => {
+            setCategoryBtn("all");
+            setFilteredItems(allProducts.products);
+          }}
         >
           All
         </button>
         {categories.map((category, index) => (
           <button
-            onClick={() => updateCategory(category)}
+            onClick={() => {
+              setCategoryBtn(category);
+              updateCategory(category);
+            }}
             key={index}
             style={{
               color: "white",
@@ -93,11 +222,30 @@ export default function Products() {
         ))}
       </div>
       <div className="filter">
+        <div>
+          <label>
+            Min Price:
+            <input
+              type="number"
+              value={minPrice}
+              onChange={(e) => setMinPrice(e.target.value)}
+            />
+          </label>
+          <label>
+            Max Price:
+            <input
+              type="number"
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+          </label>
+          <button onClick={handleFilter}>Apply Filter</button>
+        </div>
         {/* Check if filteredProducts is not empty */}
         {filteredItems.length > 0 ? (
           filteredItems.map((data) => <Card data={data} key={data.id} />)
         ) : (
-          <p>No products found for the selected category.</p>
+          <p>No products found for the selected category and price range.</p>
         )}
       </div>
     </>
